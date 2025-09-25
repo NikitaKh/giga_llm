401906f2d3ece247e948d4459934dd0165327537
Nikita Khvalin <Khvalin.N.A@sberbank.ru>
2025-09-25 22:33:11 +0300

    ref

--- app/routers/llm.py
+++ app/routers/llm.py
@@ -1,15 +1,15 @@
 from fastapi import APIRouter, Form, Depends, HTTPException, status
 from fastapi.responses import JSONResponse
 
+from app.routers import get_current_user, User
 from app.services.gigachat import generate_response_with_system_prompt
 from app.settings import settings
-from app.routers import get_current_user, User
 
 router = APIRouter(prefix='/gigachat', tags=['gigachat'])
 
 
 @router.post("/question")
-async def analysis_logs(question: str =  Form(...), current_user: User = Depends(get_current_user)) -> JSONResponse:
+async def analysis_logs(question: str = Form(...), current_user: User = Depends(get_current_user)) -> JSONResponse:
     if current_user.get('role', None) != 'admin':
         raise HTTPException(status.HTTP_403_FORBIDDEN, 'Insufficient privileges')
     response = generate_response_with_system_prompt(
